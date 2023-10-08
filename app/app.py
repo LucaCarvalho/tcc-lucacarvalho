3d93b34ba5d30429867e1ef4ef5f293409437f7b
Luca Carvalho <lucarlcarvalho@outlook.com>
2023-10-08 02:43:46 -0300

    execution logic moved to the new App class

---
+++ app/app.py
@@ -0,0 +1,32 @@
+from time import sleep
+from storage import LocalStorage, RemoteStorage
+from logging import getLogger
+from signal import signal, SIGINT
+
+logger = getLogger('app')
+
+UPDATE_INTERVAL = 15
+
+class App:
+    def __init__(self, local_storage: LocalStorage, remote_storage: RemoteStorage):
+        self._local_storage = local_storage
+        self._remote_storage = remote_storage
+        self._running = True
+        signal(SIGINT, self._handle_signal)
+
+    def loop(self):
+        while self._running:
+            logger.debug(f'Local: {self._local_storage.list_files()}')
+            logger.debug(f'Remote: {self._remote_storage.list_files()}')
+
+            logger.info(f'Syncing up... {self._local_storage.sync_up(self._remote_storage).stdout}')
+
+            logger.debug(f'Local: {self._local_storage.list_files()}')
+            logger.debug(f'Remote: {self._remote_storage.list_files()}')
+
+            sleep(UPDATE_INTERVAL)
+
+    def _handle_signal(self, signal, stack_trace) -> None:
+        logger.info('Exiting gracefully...')
+        self._running = False
+    
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import os
 import click
+import logging
+import signal
 
 from dotenv import get_variables
 from storage import RemoteStorage, LocalStorage
+from app import App
 
-import logging
 
 # Configure logging
 logging.basicConfig(format='[%(levelname)s] %(message)s', level=logging.DEBUG)
@@ -12,25 +14,22 @@ logging.getLogger('boto3').setLevel(logging.INFO)
 logging.getLogger('botocore').setLevel(logging.INFO)
 logging.getLogger('s3transfer').setLevel(logging.INFO)
 logging.getLogger('urllib3').setLevel(logging.INFO)
-logger = logging.getLogger('main')
+logger = logging.getLogger('app')
 
+
+# Load configurations
 get_variables('.env') #TODO create setup tool to generate .env
 
+# Main program
 @click.command()
 @click.option('--local_path', default=os.getcwd())       
-@click.option('--bucket', default=os.getenv('BUCKET_NAME'))       
+@click.option('--bucket', default=os.getenv('BUCKET_NAME'))
 def main(local_path: str, bucket: str) -> None:
     local = LocalStorage(local_path)
     remote = RemoteStorage(bucket)
 
-    logger.debug(local.list_files())
-    logger.debug(remote.list_files())
-
-    logger.info(f'Syncing up... {local.sync_up(remote).stdout}')
-    # logger.info(f'Syncing down... {remote.sync_down(local).stdout}')
-
-    logger.debug(local.list_files())
-    logger.debug(remote.list_files())
+    app = App(local, remote)
+    app.loop()
 
 if __name__ == '__main__':
     main()
